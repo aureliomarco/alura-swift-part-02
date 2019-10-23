2d02b04c89f7d1379e5327c18163869ede62c3e4
Marco Aurélio <marco.goncalves@ab-inbev.com>
2019-10-23 00:41:52 -0300

    Fugindo de um padrão e os delegates

--- egg/egg/Controllers/RefeicoesTableViewController.swift
+++ egg/egg/Controllers/RefeicoesTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RefeicoesTableViewController: UITableViewController, ViewControllerDelegate {
+class RefeicoesTableViewController: UITableViewController, AdicionaRefeicaoDelegate {
     
     var refeicoes = [Refeicao(nome: "Macarrão", felicidade: 2),
                      Refeicao(nome: "Pizza", felicidade: 4),
